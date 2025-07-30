90b1bb397885c0354a6f212563458d78c2323f69
Mehdi <180872863+entlv@users.noreply.github.com>
2025-07-30 22:51:32 +0000

    Create degg.cpp

---
+++ degg.cpp
@@ -0,0 +1,25 @@
+wiksdv jdfgodsj;sd;
+
+
+
+gpppppppppgorgore         
+
+
+
+
+
+oaojgakg 
+re
+jg er
+erg eiri e
+rg etig 
+w i i erii er 
+
+er
+
+er oqerro o er
+
+
+er 
+
+erkkkfnvxnb,xmcbnlkrjbx nblnflkxfrb dlxfkbjxlbd
