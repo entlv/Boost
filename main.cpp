8bdf3541fb8f9288c59fabe94c86d1d0fda821d6
Mehdi <180872863+entlv@users.noreply.github.com>
2025-08-06 18:26:14 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string s = "12-34-XX";
+    cout << s[6];
+    cout << s[7];
+    return 0;
+}
---
+++ toy.py
@@ -0,0 +1,4 @@
+print("hello")
+x=1
+for x in range(10000):
+    print(x)
